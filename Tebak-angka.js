58a19bdee260b2859e73c8db4acf4f594367d18e
Veronica Maya Santi <veronicamayasanti@gmail.com>
2023-05-25 20:37:08 +0700

    Web Storage

---
+++ Tebak-angka.js
@@ -0,0 +1,79 @@
+
+  //inisialiasi variabel untuk menampung elemen dokumen
+  const localTotalVictoryField = document.getElementById('local-total-victory-field');
+  const localMaximumAttemptField = document.getElementById('local-maximum-attempt-field');
+  const destroyDataButton = document.getElementById('destroy-data-button');
+  const playButton = document.getElementById('play-button');
+  const beforeGameDisplay = document.getElementById('before-game-display');
+  const duringGameDisplay = document.getElementById('during-game-display');
+  const afterGameDisplay = document.getElementById('after-game-display');
+  const answerButton1 = document.getElementById('answer-1-button');
+  const answerButton2 = document.getElementById('answer-2-button');
+  const answerButton3 = document.getElementById('answer-3-button');
+  const sessionUserAnswerField = document.getElementById('session-user-answer-field');
+  const sessionUserWrongAnswerField = document.getElementById('session-user-wrong-answer-field');
+  const sessionTrueAnswerField = document.getElementById('session-true-answer-field');
+  const sessionUserAttemptsField = document.getElementById('session-user-attempts-amount-field');
+  
+  //inisialisasi fungsi untuk menghasilkan jawaban permainan
+  function getAnswer() {
+    let answer = '123'.split('');
+    for (let i = 0; i < answer.length; i++) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let tmp = answer[i];
+      answer[i] = answer[j];
+      answer[j] = tmp;
+    }
+    return answer.join('');
+  }
+  
+  //inisialiasi key untuk session storage
+  const sessionAnswerKey = 'SESSION_ANSWER';
+  const sessionUserAttemptsKey = 'SESSION_USER_ATTEMPTS';
+  const sessionUserIsPlayingKey = 'SESSION_USER_IS_PLAYING';
+  
+  //inisialisasi key untuk local storage
+  const localTotalVictoryKey = 'LOCAL_TOTAL_VICTORIES_PLAYED';
+  const localMaximumAttemptsKey = 'LOCAL_MAXIMUM_ATTEMPTS';
+
+
+  window.addEventListener('load', function () {
+    if (typeof (Storage) !== 'undefined') {
+      // (MATERI 1)
+      // inisialisasi semua item web storage yang kita akan gunakan jika belum ada
+      if (sessionStorage.getItem(sessionAnswerKey) === null) {
+        sessionStorage.setItem(sessionAnswerKey, '');
+      }
+      if (sessionStorage.getItem(sessionUserAttemptsKey) === null) {
+        sessionStorage.setItem(sessionUserAttemptsKey, 0);
+      }
+      if (sessionStorage.getItem(sessionUserIsPlayingKey) === null) {
+        sessionStorage.setItem(sessionUserIsPlayingKey, false);
+      }
+      if (localStorage.getItem(localTotalVictoryKey) === null) {
+        localStorage.setItem(localTotalVictoryKey, 0);
+      }
+      if (localStorage.getItem(localMaximumAttemptsKey) === null) {
+        localStorage.setItem(localMaximumAttemptsKey, 0);
+      }
+    } else {
+      alert('Browser yang Anda gunakan tidak mendukung Web Storage');
+    }
+
+    //inisialisasi semua nilai field pada dokumen yang menggunakan nilai dari web storage
+  sessionUserAttemptsField.innerText = sessionStorage.getItem(sessionUserAttemptsKey);
+  localTotalVictoryField.innerText = localStorage.getItem(localTotalVictoryKey);
+  localMaximumAttemptField.innerText = localStorage.getItem(localMaximumAttemptsKey);
+  
+  });
+
+
+  
+
+  
+// catatan
+
+// Variabel-variabel yang bisa kita gunakan untuk berinteraksi dengan DOM.
+// Sebuah fungsi getAnswer() yang berfungsi untuk menghasilkan kombinasi dari angka "1", "2", dan "3". Kombinasi tersebut akan menjadi jawaban untuk permainan tebak angka.
+// Variabel-variabel yang menampung key untuk session storage maupun local storage.
+
---
+++ form-user-data.js
@@ -0,0 +1,95 @@
+ const storageKey = 'STORAGE_KEY';
+  const submitAction = document.getElementById('form-data-user');
+
+
+//   memeriksa apakah fitur web storage didukung oleh browser yang kita gunakan melalui sebuah fungsi bernama checkForStorage()
+
+function checkForStorage(){
+    return typeof (storage) !== 'undefined';
+}
+
+// fungsi checkForStorage() akan mengembalikan nilai true jika fitur web storage didukung oleh browser dan false jika tidak.
+
+
+// fungsi putUserList() berguna untuk membuat item storage
+// membuat nilai awal serta memodifikasi nilai pada item storage-nya
+function putUserList(data) {
+    if(checkForStorage()) {
+        let userData = [];
+        if(localStorage.getItem(storageKey) !== null ){
+            userData = JSON.parse(localStorage.getItem(storageKey));
+        }
+
+        userData.unshift(data);
+        if(userData.length > 5){
+            userData.pop();
+        }
+
+        localStorage.setItem(storageKey, JSON.stringify(userData));
+    }
+}
+
+// fungsi di atas memeriksa apakah fitur web storage sudah didukung melalui fungsi checkForStorage()
+// Jika iya, kita akan membuat sebuah variabel bernama userData yang akan menampung semua data pada item storage.
+// Jika item storage yang digunakan belum dibuat, kita akan memberikan nilai array kosong ke variabel userData
+// Jika tidak, kita akan mengambil data yang disimpan dan memasukkannya ke fungsi JSON.parse().
+//  JSON.parse()? Fungsi tersebut berguna untuk mengonversi sebuah JSON yang ditulis dalam bentuk string ke bentuk JSON "asli"
+
+
+
+
+// getUserList() yang berguna untuk mendapatkan semua data pada item storage yang berisi data user yang sudah di-input
+function getUserList() {
+    if (checkForStorage()) {
+      return JSON.parse(localStorage.getItem(storageKey)) || [];
+    } else {
+      return [];
+    }
+  }
+
+
+// menambahkan fungsi untuk merender data user pada tabel HTML
+function renderUserList() {
+    const userData = getUserList();
+    const userList = document.querySelector('#user-list-detail');
+    userList.innerHTML = '';
+    for (let user of userData) {
+      let row = document.createElement('tr');
+      row.innerHTML = '<td>' + user.nama + '</td>';
+      row.innerHTML += '<td>' + user.umur + '</td>';
+      row.innerHTML += '<td>' + user.domisili + '</td>';
+      userList.appendChild(row);
+    }
+  } 
+
+//Kemudian kita akan menambahkan event listener ke tombol submit untuk mengambil semua data yang sudah di-input ke semua field di form.
+// Lalu kita akan menyimpannya pada item storage melalui fungsi putUserList().
+// Selanjutnya daftar user yang baru saja kita masukkan akan langsung ditampilkan melalui fungsi renderUserList()
+
+ submitAction.addEventListener('submit', function (event) {
+    const inputNama = document.getElementById('nama').value;
+    const inputUmur = document.getElementById('umur').value;
+    const inputDomisili = document.getElementById('domisili').value;
+    const newUserData = {
+      nama: inputNama,
+      umur: inputUmur,
+      domisili: inputDomisili,
+    }
+    putUserList(newUserData);
+    renderUserList();
+  });
+
+// kita akan menambahkan event listener ke objek window untuk event "load"
+// Event handler-nya akan berisi perintah untuk menampilkan semua data yang sudah kita input ke dalam item storage.
+
+window.addEventListener('load', function () {
+    if (checkForStorage) {
+      if (localStorage.getItem(storageKey) !== null) {
+        renderUserList();
+      }
+    } else {
+      alert('Browser yang Anda gunakan tidak mendukung Web Storage');
+    }
+  });
+
+  
